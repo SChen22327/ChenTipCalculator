ca0355aefaba8b1a435152d63b348b643a4ed0b8
BT_1N3_15 <sample@bths.edu>
2023-09-21 09:01:44 -0400

    fixed some code, added some code to test lists work(they do)

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -7,12 +7,15 @@ import java.util.ArrayList;
     link for italicizing text
    (3) https://www.w3schools.com/java/java_arraylist.asp
     link for arraylist
+   (4) https://www.javatpoint.com/java-break
+     link for breaking loop
  */
 public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayList<String> itemNames = new ArrayList<String>();
         ArrayList<Double> itemCost = new ArrayList<Double>();
+        double totalCost = 0;
 
         System.out.print("Welcome to the tip calculator!");
         System.out.print("How many people are dining today?");
@@ -31,14 +34,27 @@ public class TipCalculator {
         System.out.print("The name of the item: ");
         String item = scan.nextLine();
 
+        itemCost.add(cost);
+        itemNames.add(item);
+        totalCost += cost;
         while (cost != -1) {
-            itemCost.add(cost);
-            itemNames.add(item);
             System.out.print("Enter another item in dollars and cents, enter -1 to end: ");
             cost = scan.nextDouble();
+            scan.nextLine();
+
+            if(cost == -1) {
+                break;
+            }
+
             System.out.print("The name of the next item: ");
             item = scan.nextLine();
-        }
 
+            itemCost.add(cost);
+            itemNames.add(item);
+            totalCost += cost;
+        }
+        System.out.println(itemCost);
+        System.out.println(itemNames);
+        System.out.print(totalCost);
     }
 }
