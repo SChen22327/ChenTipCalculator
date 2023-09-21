d3b93cd47d88c7b4da4fb31c575cd3da8ed17dcf
BT_1N3_15 <sample@bths.edu>
2023-09-21 08:53:57 -0400

    added while loop and arraylists

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 /* (1) https://stackoverflow.com/questions/11701399/round-up-to-2-decimal-places-in-java
     link for rounding to two decimals
    (2) https://stackoverflow.com/questions/30310147/how-to-print-an-string-variable-as-italicized-text
     link for italicizing text
-* */
+   (3) https://www.w3schools.com/java/java_arraylist.asp
+    link for arraylist
+ */
 public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> itemNames = new ArrayList<String>();
+        ArrayList<Double> itemCost = new ArrayList<Double>();
 
         System.out.print("Welcome to the tip calculator!");
         System.out.print("How many people are dining today?");
@@ -18,7 +24,21 @@ public class TipCalculator {
         double percentTip = scan.nextDouble();
         scan.nextLine();
 
+        System.out.print("The cost of the item in dollars and cents(enter -1 to end): ");
+        double cost = scan.nextDouble();
+        scan.nextLine();
 
+        System.out.print("The name of the item: ");
+        String item = scan.nextLine();
+
+        while (cost != -1) {
+            itemCost.add(cost);
+            itemNames.add(item);
+            System.out.print("Enter another item in dollars and cents, enter -1 to end: ");
+            cost = scan.nextDouble();
+            System.out.print("The name of the next item: ");
+            item = scan.nextLine();
+        }
 
     }
 }
